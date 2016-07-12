419b1de06a6b644c47ac92269e5a1738e9047361
Lucas <lucas@airbears2-10-142-80-190.airbears2.1918.berkeley.edu>
2016-07-12 14:42:32 -0700

    deleting templates

--- flaskr.py
+++ flaskr.py
@@ -12,47 +12,6 @@ app.config.from_object(__name__)
 orderedClusters = []
 codes = {}
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print ('Initialized the database.')
-
 @app.route('/')
 def show_entries():
     # db = get_db()
@@ -78,7 +37,7 @@ def show_entries():
         emp = codes.get(fix, [])
         emp.append( (i['before'], i['after']))
         codes[fix] = codes.get(fix, emp)
-      
+
 
     beforeExample = data[0]['before']
     afterExample = data[0]['after']
@@ -128,56 +87,6 @@ def show_detail(cluster_id):
 
 
 
-
-
-
-
-
-
-# def code(name, submit, bid):
-#     assign = get_assignment(name)
-#     backup = Backup.query.get(bid)
-#     if not (backup and backup.submit == submit and
-#             Backup.can(backup, current_user, "view")):
-#         abort(404)
-#     diff_type = 'full'
-    
-#     # highlight files and add comments
-#     files = highlight.diff_files(assign.files, backup.files(), diff_type)
-#     return render_template('student/assignment/code.html',
-#         course=assign.course, assignment=assign, backup=backup,
-#         files=files, diff_type=diff_type)
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
     # initdb_command()
