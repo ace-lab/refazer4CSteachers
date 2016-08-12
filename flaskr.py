1d46398155e47d3832d89a1e4cb8a55f4ffabec3
Lucas <lucas@Lucass-MacBook-Pro.local>
2016-08-12 15:12:28 -0700

    adding db support

--- flaskr.py
+++ flaskr.py
@@ -13,6 +13,47 @@ orderedClusters = []
 current_question = []
 questions = {1:'accumulate-mistakes.json', 2:'G-mistakes.json', 3:'Product-mistakes.json', 4:'repeated-mistakes.json'}
 
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 def prepare(question_number):
     global codes
     global orderedClusters
@@ -43,9 +84,18 @@ def prepare(question_number):
         item = (x, dict.get(x))
         if(type(item[0]) == type(u'unicode')):
             aux = item[0].replace("\\", "")
-            orderedClusters.append((aux, item[1], aux.count("Insert"), aux.count("Update"), aux.count("Delete")))
-        else:
-            orderedClusters.append(item)
+            codePair = codes[aux][0]
+
+            beforeMap = {"example":codePair[0]}
+            afterMap = {"example":codePair[1]}
+
+            filesSample = highlight.diff_files(beforeMap, afterMap, 'full')
+            for lines in filesSample.values():
+                for line in lines:
+                    line.contents = line.contents.replace("<", "&lt")
+                    line.contents = line.contents.replace(">", "&gt");
+
+            orderedClusters.append((aux, item[1], aux.count("Insert"), aux.count("Update"), aux.count("Delete"), filesSample.values()))
 
     orderedClusters = sorted(orderedClusters, key=lambda cluster: -cluster[1])
 
@@ -53,29 +103,27 @@ def prepare(question_number):
 @app.route('/<int:question_number>')
 def show_question(question_number):
     prepare(question_number)
-
-    return render_template('layout.html', question_name = questions[question_number], question_number = question_number, clusters = orderedClusters, files = [], rule = "")
+    return render_template('layout.html', question_name = questions[question_number], question_number = question_number, clusters = orderedClusters, files = [], rule = "", entries = [], cluster_id = -1)
 
 
 @app.route('/')
 def show_entries():
-
     prepare(1)
-
-    return render_template('layout.html', question_name = questions[1], question_number = 1, clusters = orderedClusters, files = [], rule = "")
+    return render_template('layout.html', question_name = questions[1], question_number = 1, clusters = orderedClusters, files = [], rule = "", entries = [], cluster_id = -1)
 
 @app.route('/<int:question_number>/<int:cluster_id>')
 def show_detail(question_number, cluster_id):
-    print("aqui")
-    print(url_for('show_detail', question_number=1, cluster_id=1))
+    print(url_for('show_detail', question_number=1, cluster_id=0))
     global codes
     global orderedClusters
+
+    db = get_db()
+    cur = db.execute('select title, cluster_id, text from entries order by id desc')
+    entries = cur.fetchall()
 
     if(question_number != current_question):
         prepare(question_number)
 
-
-    # print(orderedClusters)
 
     beforeMap = {}
     afterMap = {}
@@ -96,11 +144,19 @@ def show_detail(question_number, cluster_id):
             line.contents = line.contents.replace(">", "&gt");
 
 
+    return render_template('layout.html', question_name = questions[question_number], question_number = question_number, clusters = orderedClusters, files = files.values(), rule = fix, entries = entries, cluster_id=cluster_id)
 
-    return render_template('layout.html', question_name = questions[question_number], question_number = question_number, clusters = orderedClusters, files = files.values(), rule = fix)
+@app.route('/add', methods=['POST'])
+def add_tip():
 
-
-
+    db = get_db()
+    db.execute('insert into entries (title, cluster_id, text) values (?, ?, ?)',
+                 ['title', request.form['cluster_id'], request.form['text']])
+    db.commit()
+    print("Aqui")
+    print(request.form['cluster_id'])
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_detail', question_number=current_question, cluster_id=request.form['cluster_id']))
 
 if __name__ == '__main__':
     # initdb_command()
---
+++ schema.sql
@@ -0,0 +1,7 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  cluster_id integer not null,
+  'text' text not null
+);
