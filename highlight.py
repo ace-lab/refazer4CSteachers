90ebc5ed671693495000e045660e54c3597e6011
Ryo Suzuki <ryosuzk@gmail.com>
2016-08-26 15:26:39 -0600

    Refactor data structure

--- highlight.py
+++ highlight.py
@@ -117,3 +117,16 @@ def diff_files(files_before, files_after, diff_type, inputoutputID):
         files = {filename: list(highlight_file(filename, source))
             for filename, source in files_after.items()}
     return files
+
+def diff_file(filename, file_before, file_after, diff_type):
+    if diff_type:
+        lines = highlight_diff(
+            filename,
+            file_before,
+            file_after,
+            diff_type
+        )
+    else:
+        lines = []
+    return lines
+
