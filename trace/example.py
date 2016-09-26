2f073340a06c8b1e928cd40d67c9eec917f0f4e2
Ryo Suzuki <ryosuzk@gmail.com>
2016-09-26 15:50:17 -0600

    Get sample data

--- trace/example.py
+++ trace/example.py
@@ -6,8 +6,8 @@ import pprint
 pp = pprint.PrettyPrinter(depth=2)
 
 def finalizer(output_list):
-  # output_json = json.dumps(output_list)
-  pp.pprint(output_list)
+  output_json = json.dumps(output_list)
+  print(output_json)
 
 file = open('test/test-1.py', 'r')
 script_str = file.read()
@@ -16,6 +16,7 @@ print(script_str)
 print('')
 
 print('===== Output trace =====')
-pg_logger.exec_script_str(script_str, finalizer)
+results = pg_logger.exec_script_str(script_str, finalizer)
+
 
 
