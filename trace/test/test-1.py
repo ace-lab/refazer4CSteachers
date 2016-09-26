bf6daa514ae70a54bc9c782b3978581fc9b63f43
Ryo Suzuki <ryosuzk@gmail.com>
2016-09-26 14:41:22 -0600

    Test with the original accumulate function

--- trace/test/test-1.py
+++ trace/test/test-1.py
@@ -1,15 +1,16 @@
-from operator import add, mul
-
 def accumulate(combiner, base, n, term):
-    if n==1:
-        return base+term(1)
-    elif n==0:
-        return base+term(0)
-    else:
-        return combiner(term(n), accumulate(combiner, base, n-1, term))
+  if n==1:
+    return base+term(1)
+  elif n==0:
+    return base+term(0)
+  else:
+    return combiner(term(n), accumulate(combiner, base, n-1, term))
+
+def mul(a, b):
+  return a * b
 
 def square(x):
-    return x * x
+  return x * x
 
 res = accumulate(mul, 2, 3, square)
 print(res)
