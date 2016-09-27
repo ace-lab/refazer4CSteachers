9cfe73106342b6c78371739ab38a3d283f4ff92b
Andrew Head <head.andrewm@gmail.com>
2016-09-26 20:48:35 -0700

    Tester: Made methods for lightweight on-server auto-grading

--- flaskr.py
+++ flaskr.py
@@ -7,6 +7,9 @@ import json
 import highlight
 import requests
 import math
+import sys
+from io import StringIO
+
 
 class Rule_and_test:
     def __init__(self, test, rule):
@@ -442,6 +445,101 @@ def show_detail(question_number, tab_id, cluster_id, filter=None):
 #     db.execute('delete from entries where cluster_id=' + request.form['cluster_id'] + ' and question_number=' + request.form['question_number'])
 #     db.commit()
 #     return redirect(url_for('show_detail', question_number=request.form['question_number'], tab_id=0 cluster_id=request.form['cluster_id']))
+
+
+def evaluate_function(code_text, function_name, input_value_tuples, expected_outputs):
+
+    results = {
+        'overall_success': False,
+        'test_cases': []
+    }
+
+    # Compute the result of each individual test
+    for test_index in range(len(input_value_tuples)):
+        results['test_cases'].append(evaluate_function_once(
+            code_text=code_text,
+            function_name=function_name,
+            input_values=input_value_tuples[test_index],
+            expected_output=expected_outputs[test_index],
+        ))
+    
+    # Compute the overall success across all tests
+    results['overall_success'] = all(r['success'] for r in results['test_cases'])
+
+    return results
+
+def evaluate_function_once(code_text, function_name, input_values, expected_output):
+
+    cant_run_code = False
+    result = {
+        'input_values': input_values,
+        'expected': expected_output,
+        'success': False,  # we assume the test case failed until it completely succeeds
+    }
+
+    # Save the original stdout so we can resume them after this function runs
+    original_stdout = sys.stdout
+
+    # Compile the code to runnable form
+    try:
+        code = compile(code_text, '<string>', 'exec')
+        result['compile_success'] = True
+    except SyntaxError as s:
+        result['compile_success'] = False
+        result['syntax_error'] = {
+            'lineno': s.lineno,
+            'offset': s.offset,
+            'msg': s.msg,
+            'text': s.text,
+        }
+        cant_run_code = True
+
+    if cant_run_code is False:
+
+        # Set up fresh scopes for the code to run within
+        local_scope = {}
+        global_scope = {}
+
+        # Run the code to capture the function definition
+        try:
+            exec(code, global_scope, local_scope)
+            result['exec_success'] = True
+        except Exception as e:
+            result['exec_success'] = False
+            result['exec_exception'] = {
+                'type': type(e),
+                'args': e.args,
+            }
+            cant_run_code = True
+
+    if cant_run_code is False:
+
+        # Create a new version of stdout to capture what gets printed
+        capturable_stdout = StringIO()
+        sys.stdout = capturable_stdout
+        
+        # Execute the code with the test inputs
+        try:
+            output = local_scope[function_name](*input_values)
+            result['runtime_success'] = True
+        except Exception as e:
+            result['runtime_success'] = False
+            result['exception'] = {
+                'type': type(e),
+                'args': e.args,
+            }
+
+        # Transfer the results to the return value
+        if result['runtime_success']:
+            result['returned'] = output
+            result['success'] = (output == expected_output)
+
+        result['stdout'] = capturable_stdout.getvalue()
+
+    # Return stdout to original
+    sys.stdout = original_stdout
+    
+    return result
 
 @app.route('/submit', methods=['POST'])
 def submit_code():
